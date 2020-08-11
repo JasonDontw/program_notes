4cfd64fccfef8ed2f0c9c2bb1ef7d41c89e077b4
Jason Dong <ee13751@gmail.com>
2020-08-11 14:51:38 +0800

    20200811

--- react/udemy/1.jsx/src/index.js
+++ react/udemy/1.jsx/src/index.js
@@ -1,6 +1,7 @@
 // Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
+import TEST from './App';
 
 function gettime(){
     return (new Date()).toLocaleTimeString();
@@ -10,17 +11,22 @@ function gettime(){
 const App = function(){
     return <div>hi there!</div>
 };
+
 const App2 = ()=>{
-    const buttonText ={text:'Click me!'};
-    const style ={background:'blue',color:'white'}
+    const buttonText = {text:'Click me!'};
+    const style = {background:'blue',color:'white'}
 
     return (
     <div>
+        <App/>
+        <TEST/>
         <label className="label" htmlFor="name">Enter name:</label>
         <input type="text" id="name"/>
+
         <button style={{background:'blue',color:'white'}}>
         {gettime()}
         </button> 
+
         <button style={style}>
         {buttonText.text}
         </button> 
---
+++ react/幕課網/todolist/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          hello world!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer" //避免新開網頁影響原本網頁
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
